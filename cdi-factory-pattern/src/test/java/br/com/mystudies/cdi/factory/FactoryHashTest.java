480310f012a6fcffc09b325dfa8b10d60e617e09
Robson Duarte <robson.o.d@gmail.com>
2015-04-21 22:27:34 -0300

    add base class test tu factoryHash

---
+++ cdi-factory-pattern/src/test/java/br/com/mystudies/cdi/factory/FactoryHashTest.java
@@ -0,0 +1,18 @@
+package br.com.mystudies.cdi.factory;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FactoryHashTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
