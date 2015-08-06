198befa07215ecc663378a0ce5cfea5aeef16733
Robson Duarte <robson.o.d@gmail.com>
2015-08-05 22:07:38 -0300

    remove the class to init example again.

--- cdi-decorator-pattern/src/main/java/br/com/mystudies/cdi/decorator/Appender.java
+++
@@ -1,9 +0,0 @@
-package br.com.mystudies.cdi.decorator;
-
-import br.com.mystudies.cdi.domain.NotaFiscal;
-
-public interface Appender {
-
-	NotaFiscal append(NotaFiscal notaFiscal);
-
-}
--- cdi-decorator-pattern/src/main/java/br/com/mystudies/cdi/domain/NotaFiscal.java
+++ cdi-decorator-pattern/src/main/java/br/com/mystudies/cdi/domain/NotaFiscal.java
@@ -2,21 +2,6 @@ package br.com.mystudies.cdi.domain;
 
 public class NotaFiscal {
 
-	public Emitente emitente;
-
-
-
-
-
-
-
-	public Emitente emitente() {
-		return emitente;
-	}
-
-
-
-
 
 
 }
