d63fd5b47a29c8a90c60018e1fd918374e542eb4
robsonoduarte <robson.o.d@gmail.com>
2015-09-28 16:28:41 -0300

    add the basic concepts of CDI intercepter in first validators.

--- cdi-interceptor/src/main/java/br/com/mystudies/cdi/interceptor/ValidatorOne.java
+++ cdi-interceptor/src/main/java/br/com/mystudies/cdi/interceptor/ValidatorOne.java
@@ -1,14 +1,27 @@
 package br.com.mystudies.cdi.interceptor;
 
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
+@Interceptor
+@Validator
 public class ValidatorOne {
 
 	
 	
+	
+	
+	@AroundInvoke
 	public Object valid(InvocationContext context) {
 		return null;
 	}
 
+	
+	
+	
+	
+	
+	
 	
 }
--- cdi-interceptor/src/test/java/br/com/mystudies/cdi/interceptor/ValidatorOneTest.java
+++ cdi-interceptor/src/test/java/br/com/mystudies/cdi/interceptor/ValidatorOneTest.java
@@ -1,26 +1,39 @@
 package br.com.mystudies.cdi.interceptor;
 
 import static org.junit.Assert.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 import javax.interceptor.InvocationContext;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 public class ValidatorOneTest {
 
 	
-	
+	@InjectMocks
 	private ValidatorOne validator;
 	
 	
+	
+	@Mock
 	private InvocationContext context;
 	
 	
 	
 	
+	@Before
+	public void setUp()throws Exception{
+		initMocks(this);
+	}
+
+
+	
 	@Test
-	public void test() {
-		Object object = validator.valid(context);
+	public void test() {				
+		validator.valid(context);
 	}
 
 	
