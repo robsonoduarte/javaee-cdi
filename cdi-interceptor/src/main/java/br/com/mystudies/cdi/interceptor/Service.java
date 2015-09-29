1a58278486520c7beb27301470b0562b4b57a693
robsonoduarte <robson.o.d@gmail.com>
2015-09-29 20:54:02 -0300

    add fake service to intercepted.

---
+++ cdi-interceptor/src/main/java/br/com/mystudies/cdi/interceptor/Service.java
@@ -0,0 +1,10 @@
+package br.com.mystudies.cdi.interceptor;
+
+public class Service {
+
+	public void save(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ cdi-interceptor/src/test/java/br/com/mystudies/cdi/interceptor/ServiceTest.java
@@ -0,0 +1,26 @@
+package br.com.mystudies.cdi.interceptor;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+public class ServiceTest {
+
+	@InjectMocks
+	private Service service;
+
+	
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+	
+	
+	@Test
+	public void test() {
+		service.save(new Entity());
+	}
+
+}
