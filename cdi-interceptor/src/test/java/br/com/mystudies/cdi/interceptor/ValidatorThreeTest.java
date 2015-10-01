ec1ba87bebd1bfecf9b754958ac4ae49eac90452
robsonoduarte <robson.o.d@gmail.com>
2015-10-01 20:58:25 -0300

    add new Validator.

---
+++ cdi-interceptor/src/test/java/br/com/mystudies/cdi/interceptor/ValidatorThreeTest.java
@@ -0,0 +1,48 @@
+package br.com.mystudies.cdi.interceptor;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import javax.interceptor.InvocationContext;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class ValidatorThreeTest {
+
+	
+	@InjectMocks
+	private ValidatorName validator;
+	
+	
+	
+	@Mock
+	private InvocationContext context;
+	
+
+	@Mock
+	private Entity Entity;
+	
+	
+	
+	@Before
+	public void setUp()throws Exception{
+		initMocks(this);
+	}
+
+
+	
+	@Test
+	public void test() throws Exception {
+		when(context.getParameters()).thenReturn(new Object[]{});
+		validator.valid(context);
+		
+		verify(context).proceed();
+	}
+
+	
+	
+}
