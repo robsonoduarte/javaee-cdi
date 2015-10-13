ef61a5effaace4bf7da14ad6b718ee4aa8df4a62
robsonoduarte <robson.o.d@gmail.com>
2015-10-12 22:05:21 -0300

    add validator exception

---
+++ cdi-interceptor/src/main/java/br/com/mystudies/cdi/interceptor/ValidatorException.java
@@ -0,0 +1,15 @@
+package br.com.mystudies.cdi.interceptor;
+
+public class ValidatorException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 8105716412052598955L;
+
+	
+	
+	public ValidatorException(String message) {
+		super(message);
+	}
+
+	
+}
